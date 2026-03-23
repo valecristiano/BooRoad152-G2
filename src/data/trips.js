a1231129d778be68bd152ea45cf662575a9282df
valecristiano <valeriamdcristiano@gmail.com>
2026-03-23 18:29:02 +0300

    add img to trips

--- src/data/trips.js
+++ src/data/trips.js
@@ -1,38 +1,51 @@
+import Tokyo from "../../public/img/tokyo.jpg";
+import Paris from "../../public/img/parigi.jpg";
+import Newyork from "../../public/img/newyork.jpg";
+import Barcellona from "../../public/img/barcellona.jpg";
+import Dubai from "../../public/img/dubai.jpg";
+import Sydney from "../../public/img/sydney.jpg";
+
 export default [
   {
     id: 1,
     destination: "Tokyo",
     start_date: "2026-04-10",
     end_date: "2026-04-18",
+    image: Tokyo,
   },
   {
     id: 2,
     destination: "Paris",
     start_date: "2026-05-02",
     end_date: "2026-05-07",
+    image: Paris,
   },
   {
     id: 3,
     destination: "New York",
     start_date: "2026-06-15",
     end_date: "2026-06-22",
+    image: Newyork,
   },
   {
     id: 4,
     destination: "Barcelona",
     start_date: "2026-07-01",
     end_date: "2026-07-06",
+    image: Barcellona,
   },
   {
     id: 5,
     destination: "Dubai",
     start_date: "2026-09-12",
     end_date: "2026-09-20",
+    image: Dubai,
   },
   {
     id: 6,
     destination: "Sydney",
     start_date: "2026-11-05",
     end_date: "2026-11-14",
+    image: Sydney,
   },
 ];
