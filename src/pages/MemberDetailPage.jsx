78090caac219c858c327ed2059e2b617a1dc48bb
leonardo-monti <monti.leonardo22@gmail.com>
2026-03-23 19:02:16 +0100

    add basic MemberDetailPage

--- src/pages/MemberDetailPage.jsx
+++ src/pages/MemberDetailPage.jsx
@@ -1,7 +1,31 @@
-export default function MemberDetailPage() {
-  return (
-    <>
-      <h2>MemberDetailPage</h2>
-    </>
-  );
+import { useParams } from "react-router-dom"
+import members from "../data/members"
+
+function MemberDetailPage() {
+
+const {id}= useParams()
+
+const member = members.find((p)=> p.id ===parseInt(id,10))
+
+if(!member){
+    return <div className= "container mt-5">
+        <h2 className="text-danger">Passenger Not Found</h2>
+    </div>
 }
+
+return (
+
+<div className="container mt-5">
+    <div className="card shadow p-4">
+        <h1 className="mb-4">{member.first_name} {member.last_name}</h1>
+        <p><strong>Codice Fiscale: {member.tax_code} </strong></p>
+        <p><strong>Telefono: {member.phone}</strong></p>
+        <p><strong>E-mail: {member.email}</strong></p>
+       
+    </div>
+</div>
+)
+}
+
+export default MemberDetailPage
+
