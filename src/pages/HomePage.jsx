93ccd0c4b7418777ab3ea89518b42e861eb6f180
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-24 09:41:16 +0100

    add link to navigate to detail

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import trips from "../data/trips";
 
 export default function HomePage() {
@@ -8,40 +9,28 @@ export default function HomePage() {
           {trips.map((trip) => {
             return (
               <div className="col">
-                <div className="card text-bg-dark">
-                  {/* <div className="layover"></div> */}
-                  <img
-                    src={trip.image}
-                    className="card-img object-fit-cover"
-                    alt={trip.destination}
-                    style={{ height: "200px" }}
-                  />
-                  <div className="card-img-overlay overlay-dark d-flex align-items-center justify-content-between">
-                    <div className="d-flex">
-                      <h5 className="card-title">{trip.destination}</h5>
-                    </div>
-                    <div>
-                      <p className="card-text">{trip.start_date}</p>
-                      <p className="card-text">{trip.end_date}</p>
+                <Link to={`/${trip.id}`}>
+                  <div className="card text-bg-dark">
+                    <img
+                      src={trip.image}
+                      className="card-img object-fit-cover"
+                      alt={trip.destination}
+                      style={{ height: "200px" }}
+                    />
+                    <div className="card-img-overlay overlay-dark d-flex align-items-center justify-content-between">
+                      <div className="d-flex">
+                        <h5 className="card-title fs-2 fw-bolder">
+                          {trip.destination}
+                        </h5>
+                      </div>
+                      <div>
+                        <p className="card-text fw-bolder">{trip.start_date}</p>
+                        <p className="card-text fw-bolder">{trip.end_date}</p>
+                      </div>
                     </div>
                   </div>
-                </div>
+                </Link>
               </div>
-
-              //   <div className="col">
-              //     <div className="card">
-              //       <img src="..." className="card-img-top" alt="..." />
-              //       <div className="card-body">
-              //         <div className="d-flex justify-content-between align-items-center">
-              //           <p className="card-text">{trip.destination}</p>
-              //           <div>
-              //             <p className="card-text">{trip.start_date}</p>
-              //             <p className="card-text">{trip.end_date}</p>
-              //           </div>
-              //         </div>
-              //       </div>
-              //     </div>
-              //   </div>
             );
           })}
         </div>
