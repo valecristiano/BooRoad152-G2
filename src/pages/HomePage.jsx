29a5e3a16b5437a5f489bab830c3d665586a2006
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-24 09:17:17 +0100

    add .col

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,22 +6,24 @@ export default function HomePage() {
       <div className="row row-cols-1 gap-3">
         {trips.map((trip) => {
           return (
-            <div className="card text-bg-dark">
-              <img
-                src={trip.image}
-                className="card-img"
-                alt={trip.destination}
-              />
-              <div className="card-img-overlay">
-                <h5 className="card-title">{trip.destination}</h5>
-                <p className="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <p className="card-text">
-                  <small>Last updated 3 mins ago</small>
-                </p>
+            <div className="col">
+              <div className="card text-bg-dark">
+                <img
+                  src={trip.image}
+                  className="card-img"
+                  alt={trip.destination}
+                />
+                <div className="card-img-overlay">
+                  <h5 className="card-title">{trip.destination}</h5>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <p className="card-text">
+                    <small>Last updated 3 mins ago</small>
+                  </p>
+                </div>
               </div>
             </div>
             //   <div className="col">
