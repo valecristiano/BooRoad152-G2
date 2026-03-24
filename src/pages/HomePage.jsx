9ab52aeea4f3cd5226f0816acee7b43c4f0dcb4b
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-24 09:27:30 +0100

    add overlay

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,45 +3,50 @@ import trips from "../data/trips";
 export default function HomePage() {
   return (
     <>
-      <div className="row row-cols-1 gap-3">
-        {trips.map((trip) => {
-          return (
-            <div className="col">
-              <div className="card text-bg-dark">
-                <img
-                  src={trip.image}
-                  className="card-img"
-                  alt={trip.destination}
-                />
-                <div className="card-img-overlay">
-                  <h5 className="card-title">{trip.destination}</h5>
-                  <p className="card-text">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </p>
-                  <p className="card-text">
-                    <small>Last updated 3 mins ago</small>
-                  </p>
+      <div className="container">
+        <div className="row row-cols-1 gap-3">
+          {trips.map((trip) => {
+            return (
+              <div className="col">
+                <div className="card text-bg-dark">
+                  {/* <div className="layover"></div> */}
+                  <img
+                    src={trip.image}
+                    className="card-img object-fit-cover"
+                    alt={trip.destination}
+                    style={{ height: "200px" }}
+                  />
+                  <div className="card-img-overlay overlay-dark">
+                    <h5 className="card-title">{trip.destination}</h5>
+                    <p className="card-text">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                    <p className="card-text">
+                      <small>Last updated 3 mins ago</small>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-            //   <div className="col">
-            //     <div className="card">
-            //       <img src="..." className="card-img-top" alt="..." />
-            //       <div className="card-body">
-            //         <div className="d-flex justify-content-between align-items-center">
-            //           <p className="card-text">{trip.destination}</p>
-            //           <div>
-            //             <p className="card-text">{trip.start_date}</p>
-            //             <p className="card-text">{trip.end_date}</p>
-            //           </div>
-            //         </div>
-            //       </div>
-            //     </div>
-            //   </div>
-          );
-        })}
+
+              //   <div className="col">
+              //     <div className="card">
+              //       <img src="..." className="card-img-top" alt="..." />
+              //       <div className="card-body">
+              //         <div className="d-flex justify-content-between align-items-center">
+              //           <p className="card-text">{trip.destination}</p>
+              //           <div>
+              //             <p className="card-text">{trip.start_date}</p>
+              //             <p className="card-text">{trip.end_date}</p>
+              //           </div>
+              //         </div>
+              //       </div>
+              //     </div>
+              //   </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
